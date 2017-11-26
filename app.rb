5c523ed7faa7777a0bd1c346df447fa6dd1d374f
mac718 <mac718@gmail.com>
2017-11-25 22:39:50 -0500

    moved helper methods into a helpers block

--- app.rb
+++ app.rb
@@ -1,6 +1,28 @@
 require 'sinatra'
 require 'pry-byebug'
 
+helpers do 
+  def computer_move
+    ['rock', 'paper', 'scissors'].sample
+  end
+
+  def determine_winner
+    moves = [@my_move, @comp_move]
+    result = ''
+    WINNING_COMBOS.each do |combo|
+      binding.pry
+      if moves == combo
+        return result = 'You Win!'
+      elsif moves[0] == moves[1]
+        return result = "It's a tie!"
+      else
+        result = 'Computer wins!'
+      end
+    end
+    result
+  end
+end
+
 get '/boss' do
   erb :boss
 end
@@ -24,25 +46,7 @@ post '/rps' do
   erb :rps_result, locals: { human_move: @my_move, computer_move: @comp_move, result: determine_winner }
 end
 
-def computer_move
-  ['rock', 'paper', 'scissors'].sample
-end
 
-def determine_winner
-  moves = [@my_move, @comp_move]
-  result = ''
-  WINNING_COMBOS.each do |combo|
-    binding.pry
-    if moves == combo
-      return result = 'You Win!'
-    elsif moves[0] == moves[1]
-      return result = "It's a tie!"
-    else
-      result = 'Computer wins!'
-    end
-  end
-  result
-end
 
 
 
